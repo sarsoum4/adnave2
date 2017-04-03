33a76b555530e55c93e5d0cf476ecb80088176fc
mahmuds <mhmod.srsor@gmail.com>
2017-04-03 20:10:29 +0300

    add State class and install MedallionPriorityQueue package

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/BestFirstSearch.cs
@@ -0,0 +1,15 @@
+﻿using Medallion.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    class BestFirstSearch
+    {
+
+        var pq = new PriorityQueue<State>();
+    }
+}
