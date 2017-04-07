0800ee6c6635e97c2b3028b10fe67f7c0e9e33b6
Lee Shoshani <alister146@gmail.com>
2017-04-07 10:35:48 +0300

    DFS done

--- SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SearchAlgorithmsLib
 {
-    class DFS<T> : Searcher<T>
+    public class DFS<T> : Searcher<T>
     {
         HashSet<State<T>> visited = new HashSet<State<T>>();
         Stack<State<T>> s = new Stack<State<T>>();
@@ -22,15 +22,35 @@ namespace SearchAlgorithmsLib
             while (!(s.Count == 0))
             {
                 State<T> v = s.Pop();
+                if (v.Equals(searchable.getGoalState())){
+                    return backTrace(searchable); // private method, back traces through the parents
+                }
                 if (!(visited.Contains(v)))
                 {
                     visited.Add(v);
+                    succerssors = searchable.getAllPossibleStates(v);
                     foreach (State<T> state in succerssors)
                     {
+                        state.CameFrom = v;
                         s.Push(state);
                     }
                 }
             }
+            return backTrace(searchable);
+        }
+
+        private Solution<T> backTrace(ISearchable<T> searchable)
+        {
+
+            Solution<T> sol = new Solution<T>();
+            State<T> goal = searchable.getGoalState();
+            State<T> curr = goal;
+            while (curr != null)
+            {
+                sol.addToSolution(curr);
+                curr = curr.CameFrom;
+            }
+            return sol;
         }
     }
 }
